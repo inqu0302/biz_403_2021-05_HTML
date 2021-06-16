0c1a4b24501c86b30b9b9e01d761341dfb54c7e0
inqu0302 <inqu0302@naver.com>
2021-06-16 11:50:00 +0900

    06-16

--- Team_Project/position.js
+++ Team_Project/position.js
@@ -1,7 +1,11 @@
+let lat, lng
+
 // 요청 수락시
 function success(position){
-    let lat = position.coords.latitude; // x 축 좌표
-    let lng = position.coords.longitude;// y 축 좌표
+    lat = position.coords.latitude; // x 축 좌표
+    lng = position.coords.longitude;// y 축 좌표
+
+    getAddr(coords)
 
 
     // DATA 확인용
@@ -15,7 +19,7 @@ function success(position){
 
     
     // string 형태로 json을 localStorage에 저장
-    localStorage.setItem('coods', JSON.stringify(coordObj)); 
+    // localStorage.setItem('coods', JSON.stringify(coordObj)); 
 
 };
 
@@ -24,20 +28,46 @@ function error(err){
     console.log("ERROR : " + err.code + " / " + err.message);
 }
 
+
+
+function getAddr(coords){
+
+    const API_id = "X-NCP-APIGW-API-KEY-ID: hshxrb7y3r";
+    const API_key = "X-NCP-APIGW-API-KEY: NDrSmaBAZlLdDNbn04WQUumXMOGkmttfBncLRaFF";
+
+    const URI = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=${lng},${lat}&orders=admcode&output=json`;
+    fetch(URI, {
+        method: 'GET',
+        // mode : "cors",
+        headers:{
+
+            "X-NCP-APIGW-API-KEY-ID" : "hshxrb7y3r",
+            "X-NCP-APIGW-API-KEY" : "cFav5snzfNqDKs0ssoAZ3CtvhW0IOVvqBHYaX7aA",
+            'Access-Control-Allow-Origin' : "no-cors"
+        }
+    }).then(function(response){
+            // alert("동작")
+     
+            return response.json();
+    }).then(function(json){
+        console.log("데이터",json)
+    }).catch(function(error){
+        //   alert("error")
+    //  {
+    //   error = {
+    //   "errorCode" : "300",
+    //   "message" : "Not Found Exception"
+    //   }
+    //   }
+        console.log(error);
+    })
+};
+
 navigator.geolocation.getCurrentPosition(success, error);
 
 let coords = (localStorage.getItem('coods')) ;
 
-alert(coords)
 
-const URI = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=${coords}&orders=admcode&output=json";
-const API_id = "X-NCP-APIGW-API-KEY-ID: hshxrb7y3r";
-const API_key = "X-NCP-APIGW-API-KEY: NDrSmaBAZlLdDNbn04WQUumXMOGkmttfBncLRaFF";
 
-function getAddr(coords){
-    fetch(URI, API_id,API_key).then(function(response){
-        return response.json();
-    }).then(function(json){
-        console.log(json)
-    })
-};
+
+
